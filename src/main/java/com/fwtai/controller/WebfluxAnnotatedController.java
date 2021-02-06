6812be998a48d035f2a53d0a4c06e5c90425cb20
gzstyp <444141300@qq.com>
2021-02-07 01:21:51 +0800

    已解决IE8请求时出现下载的bug，获取请求参数和请求头

--- src/main/java/com/fwtai/controller/WebfluxAnnotatedController.java
+++ src/main/java/com/fwtai/controller/WebfluxAnnotatedController.java
@@ -4,6 +4,7 @@ import com.fwtai.tool.ToolClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -16,19 +17,19 @@ import java.util.ArrayList;
 @RequestMapping("/annotated")
 public class WebfluxAnnotatedController{
 
-    // http://127.0.0.1:701/annotated/object/10
-    @GetMapping(value = "/object/{id}",produces = MediaType.TEXT_HTML_VALUE)//解决IE8请求时出现下载的bug
-    public Mono<String> user(@PathVariable String id){
+    // 获取请求参数,仅能接收简单的类型???复杂类型用'函数接口'来处理? http://127.0.0.1:701/annotated/get/10
+    @GetMapping(value = "/get/{id}",produces = MediaType.TEXT_HTML_VALUE)//解决IE8请求时出现下载的bug
+    public Mono<String> user(@PathVariable final String id){//可以指定name @PathVariable(name = "id") final String id
         final String json = ToolClient.json("基于注解实现Webflux,id="+id);
         return Mono.just(json);
     }
 
-    // http://127.0.0.1:701/annotated/list
+    // 获取请求头,http://127.0.0.1:701/annotated/list
     @GetMapping(value = "/list",produces = MediaType.TEXT_HTML_VALUE)//解决IE8请求时出现下载的bug
-    public Flux<String> list(){
+    public Flux<String> list(@RequestHeader(name = "token",required = false) String header){
         final ArrayList<String> list = new ArrayList<>();
         list.add("object");
-        list.add(",基于注解实现 webflux 的方式");
+        list.add(",基于注解实现 webflux 的方式,header->"+header);
         return Flux.fromIterable(list);
     }
 }
