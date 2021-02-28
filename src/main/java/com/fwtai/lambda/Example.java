2d0586414c2160b23f1a2503bc2684c4ff64c8db
gzstyp <444141300@qq.com>
2021-02-28 10:44:57 +0800

    lambda示例代码

---
+++ src/main/java/com/fwtai/lambda/Example.java
@@ -0,0 +1,46 @@
+package com.fwtai.lambda;
+
+/**
+ * lambda示例代码
+ * @作者 田应平
+ * @版本 v1.0
+ * @创建时间 2021-02-28 10:25
+ * @QQ号码 444141300
+ * @Email service@dwlai.com
+ * @官网 http://www.fwtai.com
+ */
+public final class Example{
+
+    @FunctionalInterface
+    interface Persion{
+        void say();
+        default void fun(){
+            System.out.println("我是lambda函数接口编程的默认方法,默认方法可以有多个");
+        }
+    }
+
+    public static void main(String[] args){
+        //常规的写法
+        Persion jerry = new Persion(){
+            @Override
+            public void say(){
+                System.out.println("常规的写法");
+            }
+        };
+
+        //lambda的写法1
+        Persion tom = ()->{
+            System.out.println("lambda的写法1");
+        };
+
+        //lambda的写法2,当且仅当只有一行方法体时可以省略{}
+        Persion mary = () -> System.out.println("lambda的写法2");
+
+        jerry.say();
+
+        tom.say();
+        tom.fun();
+
+        mary.say();
+    }
+}
