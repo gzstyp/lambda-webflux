3bc802e0b74e135e0ca1c862f46590ee03a50e35
gzstyp <444141300@qq.com>
2021-02-28 09:44:53 +0800

    自定义函数式接口,接口类型有且只有一个抽象方法,可以有多个非抽象方法或静态方法!!!

--- src/main/java/com/fwtai/lambda/NumberFunction.java
+++ src/main/java/com/fwtai/lambda/NumberFunction.java
@@ -1,7 +1,7 @@
 package com.fwtai.lambda;
 
 /**
- * 自定义函数式接口,接口类型有且只有一个抽象方法
+ * 自定义函数式接口,接口类型有且只有一个抽象方法,可以有多个非抽象方法或静态方法!!!
  * @作者 田应平
  * @版本 v1.0
  * @创建时间 2019-06-12 16:34
@@ -14,4 +14,19 @@ public interface NumberFunction{
 
     Integer getValue(final Integer number);
 
+    default Integer value1(){
+        return 1;
+    }
+
+    default Integer value2(){
+        return 1;
+    }
+
+    static Integer fun1(){
+        return 2;
+    }
+
+    static Integer fun2(){
+        return 2;
+    }
 }
