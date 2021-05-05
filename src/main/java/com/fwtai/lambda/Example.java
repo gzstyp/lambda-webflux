3fbd68722b35fdbf8e79349686043392a3401078
引路者 <444141300@qq.com>
2021-05-05 22:11:16 +0800

    接口类型有且只有一个抽象方法,可以有多个非抽象方法或静态方法!!!

--- src/main/java/com/fwtai/lambda/Example.java
+++ src/main/java/com/fwtai/lambda/Example.java
@@ -1,7 +1,7 @@
 package com.fwtai.lambda;
 
 /**
- * lambda示例代码
+ * lambda示例代码,接口类型有且只有一个抽象方法,可以有多个非抽象方法或静态方法!!!
  * @作者 田应平
  * @版本 v1.0
  * @创建时间 2021-02-28 10:25
@@ -13,13 +13,13 @@ public final class Example{
 
     @FunctionalInterface
     interface Persion{
-        void say();
-        default void fun(){
+        public void say();//抽象方法
+        default void fun(){//默认方法
             System.out.println("我是lambda函数接口编程的默认方法,默认方法可以有多个");
         }
     }
 
-    public static void main(String[] args){
+    public static void main(final String[] args){
         //常规的写法
         final Persion jerry = new Persion(){
             @Override
@@ -39,6 +39,7 @@ public final class Example{
         jerry.say();
 
         tom.say();
+
         tom.fun();
 
         mary.say();
