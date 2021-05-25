6a2fb59b29ef5f2a27dbc95dbea3b322f0e0e521
引路者 <444141300@qq.com>
2021-05-25 21:36:26 +0800

    函数接口方法里的 default 修饰的方法应该叫 默认是实现方法

--- src/main/java/com/fwtai/lambda/NumberFunction.java
+++ src/main/java/com/fwtai/lambda/NumberFunction.java
@@ -1,7 +1,8 @@
 package com.fwtai.lambda;
 
 /**
- * 自定义函数式接口,接口类型有且只有一个抽象方法,可以有多个非抽象方法或静态方法!!!
+ * todo 自定义函数式接口,接口类型有且只有一个抽象方法,可以有多个非抽象方法或静态方法!!!
+ *  函数接口方法里的 default 修饰的方法应该叫 默认是实现方法
  * @作者 田应平
  * @版本 v1.0
  * @创建时间 2019-06-12 16:34
@@ -13,7 +14,7 @@ package com.fwtai.lambda;
 public interface NumberFunction{
 
     Integer getValue(final Integer number);
-
+    // todo 此处的 default 是叫默认是实现方法
     default Integer value1(){
         return 1;
     }
