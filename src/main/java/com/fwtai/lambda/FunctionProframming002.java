bb96a1032bab038c699e96ec0e7847527ae46eed
引路者 <444141300@qq.com>
2021-05-25 20:55:25 +0800

    函数式编程就像使用Excel表格计算最大值、最小值、总和一样的道理,我们不关心是怎么实现的，我们仅想要什么样的结果即可

---
+++ src/main/java/com/fwtai/lambda/FunctionProframming002.java
@@ -0,0 +1,15 @@
+package com.fwtai.lambda;
+
+/**
+ * 函数式编程就像使用Excel表格计算最大值、最小值、总和一样的道理,我们不关心是怎么实现的，我们仅想要什么样的结果即可
+ * @param 
+ * @作者 田应平
+ * @QQ 444141300
+ * @创建时间 2021/5/25 20:50
+*/
+public class FunctionProframming002{
+
+    public static void main(String[] args){
+
+    }
+}
